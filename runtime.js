c70bb22c16c2c263a2a04726574c3a08e3bda809
mchaudhury <mushsalcha@gmail.com>
2021-10-01 12:36:41 -0500

    added runtime analysis

--- runtime.js
+++ runtime.js
@@ -26,23 +26,23 @@ function getSizedArray(size) {
   return array;
 }
 
-// const tinyArray = getSizedArray(10);
-// const smallArray = getSizedArray(100);
+const tinyArray = getSizedArray(10);
+const smallArray = getSizedArray(100);
 const mediumArray = getSizedArray(1000);
-// const largeArray = getSizedArray(10000);
-// const extraLargeArray = getSizedArray(100000);
+const largeArray = getSizedArray(10000);
+const extraLargeArray = getSizedArray(100000);
 
 // How long does it take to double every number in a given
 // array?
 
 // Try it with first function
 perf.start(); // Starts timer
-doublerAppend(mediumArray);
+doublerAppend(extraLargeArray);
 let resultsAppend = perf.stop(); // Stops timer and save time results
 
 // Try it with second function
 perf.start();
-doublerInsert(mediumArray);
+doublerInsert(extraLargeArray);
 let resultsInsert = perf.stop();
 
 console.log("Results for the extraLargeArray");
